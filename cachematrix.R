059cc8098cb4c6bb9d33b148ddbb75c5157d6100
root <edward>
2014-09-23 11:33:01 -0700

    check in for now

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+       
 }
 
 
-## Write a short comment describing this function
+## Return inverse of a square matrix. Get result from 
+## cache if present, if not calculate, insert to cache 
+## before returning result.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ## Basic sanity checks for input
+        stopifnot(!(is.na(x) | is.null(x) | !is.matrix(x))
+
+        ## Try get result from cache, return result if found
+        inverse <- x$getInverse()
+        if(!is.null(inverse)) {
+              message('Return inverse from cache')
+              return(inverse)
+        }
+    
+        ## Result not in cache, calculate one, 
+        ## store in cache then return result
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
+
+# Basic tests for validation
+test <- function() {
+
+
+}
+
